651bb8e9e3207b549f50736e4e8ad7c4df32e43f
murosyo01 <murozuka75@gmail.com>
2025-05-27 12:10:14 +0900

    フォーム作成

---
+++ src/main/java/com/example/form/ArticleForm.java
@@ -0,0 +1,36 @@
+package com.example.form;
+
+public class ArticleForm {
+    /**
+     * 名前
+     */
+    private String name;
+    /**
+     * コンテンツ
+     */
+    private String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "name='" + name + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
